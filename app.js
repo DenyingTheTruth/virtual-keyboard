b9d30046085ed6de306e6cae4c18e5bd6c1119b2
DenyingTheTruth <anton.bachikin@gmail.com>
2020-04-07 22:05:16 +0300

    refactor: add bind possibilities for any textinput

--- app.js
+++ app.js
@@ -1,45 +1,40 @@
-import KEY_BUTTONS from "./src/keyConfig.js";
-import Button from "./src/button.js";
-import AltButton from "./src/altButton.js";
-import LetterButton from "./src/letterButton.js";
+import KEY_BUTTONS from './src/keyConfig.js';
+import Button from './src/button.js';
+import AltButton from './src/altButton.js';
+import LetterButton from './src/letterButton.js';
 
 class Keyboard {
-  constructor() {
+  constructor(textarea) {
     this.keys = {};
-    this.lang = localStorage.getItem("lang") || "en";
-    this.textArea = document.createElement("textarea");
+    this.lang = localStorage.getItem('lang') || 'en';
+    this.textArea = textarea;
     this.capsLockState = false;
     this.shiftState = false;
     this.ctrlKeyUp = false;
 
-    this.keyboardContainer = document.createElement("div");
-    this.keyboardContainer.classList.add("keyboard-container");
+    this.keyboardContainer = document.createElement('div');
+    this.keyboardContainer.classList.add('keyboard-container');
 
-    const keyboard = document.createElement("div");
-    keyboard.classList.add("keyboard");
+    const keyboard = document.createElement('div');
+    keyboard.classList.add('keyboard');
 
-    const keyboardKeys = document.createElement("div");
-    keyboardKeys.classList.add("keyboard__keys");
-
-    const info = document.createElement("p");
-    info.innerHTML = `Change language - 'Shift' + 'Ctrl'. <br> <strong>Notes:</strong> Key 'Shift' sticks on mouse click for change language <br> Made with OS Linux (Ubuntu 18.04 LTS).`;
-    info.classList.add("info");
-
+    const keyboardKeys = document.createElement('div');
+    keyboardKeys.classList.add('keyboard__keys');
     keyboardKeys.append(this.createKeys());
-    keyboardKeys.addEventListener("click", this.trackClickHandler);
-    keyboard.append(keyboardKeys);
-    this.textArea.classList.add("textarea");
-    this.keyboardContainer.append(this.textArea, keyboard, info);
+    keyboardKeys.addEventListener('click', this.trackClickHandler);
 
-    this.keyboardContainer.addEventListener("keydown", this.addActive);
-    this.keyboardContainer.addEventListener("keyup", this.removeActive);
+    keyboard.append(keyboardKeys);
+    this.keyboardContainer.append(keyboard);
+
+    this.textArea.addEventListener('keydown', this.addActive);
+    this.textArea.addEventListener('keyup', this.removeActive);
   }
 
   createKeys = () => {
     const keyboardKeys = document.createDocumentFragment();
     KEY_BUTTONS.forEach(rowButtonItems => {
-      let row = document.createElement("div");
-      row.classList.add("keyboard__row");
+      let row = document.createElement('div');
+      row.classList.add('keyboard__row');
       for (const button of rowButtonItems) {
         const {
           text,
@@ -66,9 +61,9 @@ class Keyboard {
 
   createKeyButton = (text, width, lang, altText, type, code) => {
     switch (type) {
-      case "alternative":
+      case 'alternative':
         return new AltButton(text, width, lang, altText, code);
-      case "functional":
+      case 'functional':
         return new Button(text, width, lang, altText, code);
       default:
         return new LetterButton(text, width, lang, altText, code);
@@ -76,31 +71,31 @@ class Keyboard {
   }
 
   isCaps = (code) => {
-    return code === "CapsLock";
+    return code === 'CapsLock';
   }
 
   isShift = (code) => {
-    return ["ShiftLeft", "ShiftRight"].includes(code);
+    return ['ShiftLeft', 'ShiftRight'].includes(code);
   }
 
   isCtrl = (code) => {
-    return ["ControlRight", "ControlLeft"].includes(code);
+    return ['ControlRight', 'ControlLeft'].includes(code);
   }
 
   isArrow = (code) => {
-    return ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(code);
+    return ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(code);
   }
 
   addActive = (e) => {
     const button = this.keys[e.code];
     if (button) {
       if (this.isArrow(button.code)) {
-        button.keyNode.classList.add("active");
+        button.keyNode.classList.add('active');
       } else {
         e.preventDefault();
 
         if (this.isShift(button.code)) {
-          button.keyNode.classList.add("active");
+          button.keyNode.classList.add('active');
           this.shiftState = !this.shiftState;
 
           for (const key in this.keys) {
@@ -113,7 +108,7 @@ class Keyboard {
         }
 
         if (this.isCtrl(button.code)) {
-          button.keyNode.classList.add("active");
+          button.keyNode.classList.add('active');
           if (this.shiftState) {
             this.changeLanguage();
           }
@@ -124,7 +119,7 @@ class Keyboard {
           !this.isShift(button.code) &&
           !this.isCtrl(button.code)
         ) {
-          button.keyNode.classList.add("active");
+          button.keyNode.classList.add('active');
           button.keyNode.click();
         }
       }
@@ -135,18 +130,18 @@ class Keyboard {
     const button = this.keys[e.code];
     if (button) {
       if (this.isArrow(button.code)) {
-        button.keyNode.classList.remove("active");
+        button.keyNode.classList.remove('active');
       } else {
         e.preventDefault();
 
         if (this.isShift(button.code)) {
-          button.keyNode.classList.remove("active");
+          button.keyNode.classList.remove('active');
           this.shiftState = false;
 
           if (!this.ctrlKeyUp) {
             for (const key in this.keys) {
               this.keys[key].shift();
-              this.keys[key].keyNode.classList.remove("uppercase");
+              this.keys[key].keyNode.classList.remove('uppercase');
             }
           }
 
@@ -154,7 +149,7 @@ class Keyboard {
         }
 
         if (!this.isCaps(button.code) && !this.isShift(button.code)) {
-          button.keyNode.classList.remove("active");
+          button.keyNode.classList.remove('active');
         }
       }
     }
@@ -204,13 +199,13 @@ class Keyboard {
 
       this.updateCursor(start);
     },
-    "Caps Lock": (e) => {
+    'Caps Lock': (e) => {
       const {
         selectionStart: start
       } = this.textArea;
 
       this.capsLockState = !this.capsLockState;
-      e.target.classList.toggle("active");
+      e.target.classList.toggle('active');
 
       for (const button in this.keys) {
         this.keys[button].capsLock();
@@ -230,7 +225,7 @@ class Keyboard {
       this.updateCursor(start + 1);
     },
     Shift: (e) => {
-      e.target.classList.toggle("active");
+      e.target.classList.toggle('active');
       this.shiftState = !this.shiftState;
 
       for (const button in this.keys) {
@@ -254,24 +249,24 @@ class Keyboard {
 
       this.updateCursor(start);
     },
-    "&#8592;": () => {
+    '&#8592;': () => {
       const {
         selectionStart: start
       } = this.textArea;
 
       this.updateCursor(start - 1);
     },
-    "&#8593;": () => {
+    '&#8593;': () => {
       this.updateCursor(0);
     },
-    "&#8594;": () => {
+    '&#8594;': () => {
       const {
         selectionStart: start
       } = this.textArea;
 
       this.updateCursor(start + 1);
     },
-    "&#8595;": () => {
+    '&#8595;': () => {
       const {
         value: val
       } = this.textArea;
@@ -283,7 +278,7 @@ class Keyboard {
         selectionStart: start
       } = this.textArea;
 
-      alert("You Win, kek 🥳");
+      alert('You Win, kek 🥳');
 
       this.updateCursor(start);
     },
@@ -296,16 +291,16 @@ class Keyboard {
   }
 
   changeLanguage = () => {
-    this.lang = this.lang === "en" ? "ru" : "en";
-    localStorage.setItem("lang", this.lang);
+    this.lang = this.lang === 'en' ? 'ru' : 'en';
+    localStorage.setItem('lang', this.lang);
     for (const button in this.keys) {
       this.keys[button].setLanguage(this.lang);
 
       if (!this.isCtrl(this.keys[button].code)) {
-        this.keys[button].keyNode.classList.remove("active");
+        this.keys[button].keyNode.classList.remove('active');
       }
 
-      this.keys[button].keyNode.classList.remove("uppercase");
+      this.keys[button].keyNode.classList.remove('uppercase');
     }
     this.shiftState = false;
     this.ctrlKeyUp = true;
@@ -347,6 +342,19 @@ class Keyboard {
 }
 
 window.onload = () => {
-  const keyboard = new Keyboard();
-  document.body.append(keyboard.keyboardContainer);
+  const textarea = document.createElement('textarea');
+  textarea.classList.add('textarea');
+
+  const keyboard = new Keyboard(textarea);
+
+  const info = document.createElement('p');
+  info.innerHTML = `
+    Change language - 'Shift' + 'Ctrl'. 
+    <br> 
+    <strong>Notes:</strong> Key 'Shift' sticks on mouse click for change language 
+    <br> Made with OS Linux (Ubuntu 18.04 LTS).
+  `;
+  info.classList.add('info');
+
+  document.body.append(textarea, keyboard.keyboardContainer, info);
 };
